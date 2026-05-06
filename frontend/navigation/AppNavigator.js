a939008d97a914c9059eac1ccbe68306ca4eb0fe
Junior Fruto <jrfruto19@hotmail.com>
2026-05-06 10:29:23 -0500

    Nueva importación en AppNavigator
    Se agregó nueva importación "MapaRutaScreen" para la funcionalidad del botón "Crear Ruta"

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 import HomeScreen from "../screens/HomeScreen";
 import RutasScreen from "../screens/RutasScreen";
+import MapaRutaScreen from "../screens/MapaRutaScreen";   // ← nueva importación
 
 const Stack = createNativeStackNavigator();
 
@@ -13,12 +14,20 @@ export default function AppNavigator() {
         <Stack.Screen
           name="Inicio"
           component={HomeScreen}
+          options={{ title: "Rutas Mompox" }}   // opcional, para que el header muestre el nombre
         />
 
         <Stack.Screen
           name="Rutas"
           component={RutasScreen}
           options={{ title: "Lugares" }}
+        />
+
+        {/* Nueva pantalla de creación de rutas */}
+        <Stack.Screen
+          name="MapaRuta"
+          component={MapaRutaScreen}
+          options={{ title: "Crear Ruta" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
